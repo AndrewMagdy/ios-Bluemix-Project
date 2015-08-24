5849eae7cb7b4ec23e53d79b07969cc566e3f013
AndrewMagdy <andrewtawf@gmail.com>
2015-08-24 09:54:01 +0300

    Initial Commit

---
+++ Prototype2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prototype2
+//
+//  Created by Andrew Magdy on 248//15.
+//  Copyright (c) 2015 Andrew Magdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
